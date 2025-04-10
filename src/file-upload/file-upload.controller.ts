d9581e0bb4b7b8f868da4c5ad5667d3cfc61051a
mshahzebraza <m.shahzebraza.97@gmail.com>
2025-04-10 10:56:47 +0500

    feat/add-logic-setup-multer-interception

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('file-upload')
-export class FileUploadController {}
+export class FileUploadController {
+    @Post('/')
+    @UseInterceptors(FileInterceptor('file')) // Query: What if i don't use interceptor?
+    async uploadFile(
+        @UploadedFile(
+            new ParseFilePipe({
+                validators: [
+                    new MaxFileSizeValidator({ maxSize: 1000_000 }), // 1mb
+                    new FileTypeValidator({ fileType: 'image/jpeg' }),
+                ],
+            })
+        ) file: Express.Multer.File // Query: Where is this type coming from? Did i need to insteall the @types/multer for this?
+    ) {
+        console.log(file);
+        return;
+    }
+}

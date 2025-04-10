7ecf4bafdf1a4ae6bba81ca1e9592e36c8d7d0b2
mshahzebraza <m.shahzebraza.97@gmail.com>
2025-04-10 16:14:42 +0500

    feat: connect with s3 bucket to get, upload and delete files

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ListBucketsCommand } from '@aws-sdk/client-s3';
+import { Controller, FileTypeValidator, Get, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors, Query } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { FileUploadService } from 'src/file-upload/file-upload.service';
 
 @Controller('file-upload')
 export class FileUploadController {
+
+    constructor(private readonly fileUploadService: FileUploadService) { }
+
     @Post('/')
     @UseInterceptors(FileInterceptor('file')) // Query: What if i don't use interceptor?
     async uploadFile(
@@ -10,12 +15,24 @@ export class FileUploadController {
             new ParseFilePipe({
                 validators: [
                     new MaxFileSizeValidator({ maxSize: 1000_000 }), // 1mb
-                    new FileTypeValidator({ fileType: 'image/jpeg' }),
+                    // new FileTypeValidator({ fileType: 'image/jpeg,image/png,image/jpg' }),
                 ],
             })
         ) file: Express.Multer.File // Query: Where is this type coming from? Did i need to insteall the @types/multer for this?
     ) {
-        console.log(file);
-        return;
+        const res = await this.fileUploadService.uploadFile(file);
+        return res;
+    }
+
+    @Get('/')
+    async getFiles(
+        @Query('limit') limit?: number,
+        // @Query('page') page?: number,
+    ) {
+        const res = await this.fileUploadService.getFiles({
+            limit: limit ? Number(limit) : undefined,
+            // page: page ? Number(page) : 1,
+        });
+        return res;
     }
 }

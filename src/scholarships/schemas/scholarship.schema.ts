280a1ad0ba79e0355945224111f648105684d444
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-04-22 12:31:45 +0500

    feat: setup the student_scholarship schema

--- src/scholarships/schemas/scholarship.schema.ts
+++ src/scholarships/schemas/scholarship.schema.ts
@@ -1,65 +1,123 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Schema as MongooseSchema } from 'mongoose';
+import { Document, HydratedDocument, Schema as MongooseSchema } from 'mongoose';
 
-export type ScholarshipDocument = Scholarship & Document;
+export type StudentScholarshipDocument = HydratedDocument<StudentScholarship>
+
+interface StudentSnapshot {
+    name?: string;
+    father_name?: string;
+    father_status?: string;
+    domicile?: string;
+    monthly_household_income?: number;
+    last_degree?: {
+        level?: string;
+        percentage?: number;
+    };
+}
+
+// required documents enum
+enum RequiredDocumentTitle {
+    passport = 'passport',
+    national_id = 'national_id',
+    birth_certificate = 'birth_certificate',
+    academic_transcripts = 'academic_transcripts',
+    recommendation_letter = 'recommendation_letter',
+    personal_statement = 'personal_statement',
+    financial_statements = 'financial_statements',
+    english_proficiency_certificate = 'english_proficiency_certificate',
+    resume_cv = 'resume_cv',
+}
+
+enum ScholarshipApprovalStatus {
+    Applied = 'Applied',
+    Approved = 'Approved',
+    Rejected = 'Rejected',
+}
+
+enum LastDegreeType {
+    Matriculation = 'Matriculation',
+    IntermediateFScFA = 'Intermediate/FSc/FA',
+    Bachelors = 'Bachelors',
+    Masters = 'Masters',
+    PhD = 'PhD',
+}
+
 
 interface RequiredDocument {
-    id: string;
-    document_name: string;
+    document_name?: RequiredDocumentTitle;
+    document_link?: string;
 }
 
-@Schema({ timestamps: true, collection: 'scholarships' })
-export class Scholarship {
-    @Prop({ type: String, required: true })
-    scholarship_name: string;
+@Schema({ timestamps: false, collection: 'student_scholarships' })
+export class StudentScholarship {
+    @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User', required: false })
+    student_id?: MongooseSchema.Types.ObjectId;
 
-    @Prop({ type: String, required: true })
-    scholarship_description: string;
+    // How to refactor this?
+    @Prop({
+        type: {
+            // REVIEW: Is the default behavior to make the property required?
+            name: { type: String },
+            father_name: { type: String },
+            father_status: { type: String },
+            domicile: { type: String },
+            monthly_household_income: { type: Number },
+            last_degree: { 
+                level: {
+                    type: String,
+                    enum: LastDegreeType,
+                    required: false,
+                },
+                percentage: { type: Number, required: false },
+            },
+        },
+        default: {},
+    })
+    student_snapshot?: StudentSnapshot;
 
-    @Prop({ type: String, enum: ['merit', 'need', 'local', 'international'], default: 'merit' })
-    scholarship_type: string;
+    @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Scholarship', required: false })
+    scholarship_id?: MongooseSchema.Types.ObjectId;
 
-    @Prop({ type: Number, default: 0 })
-    amount: number;
+    @Prop({ type: Date, required: false })
+    application_date?: Date;
 
-    @Prop({ type: Date, required: true })
-    application_deadline: Date;
+    @Prop({
+        type: String,
+        enum: ScholarshipApprovalStatus,
+        required: false
+    })
+    approval_status?: ScholarshipApprovalStatus; 
 
-    @Prop({ type: String })
-    application_link?: string;
 
-    @Prop({ type: String })
-    application_process?: string;
+    @Prop({ type: String, required: false })
+    personal_statement?: string;
 
-    @Prop({ type: String, required: true })
-    eligibility_criteria: string;
+    @Prop({ type: String, required: false })
+    reference_1?: string;
 
-    @Prop({ type: [Object], default: [] })
-    required_documents: RequiredDocument[];
+    @Prop({ type: String, required: false })
+    reference_2?: string;
 
-    @Prop({ type: String, enum: ['open', 'closed'], default: 'open' })
-    status: string;
-
-    @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'University', required: true })
-    university_id: MongooseSchema.Types.ObjectId;
-
-    // @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Country' })
-    // country?: MongooseSchema.Types.ObjectId;
-
-    @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Region' })
-    region?: MongooseSchema.Types.ObjectId;
-
-    @Prop({ type: String })
-    image_url?: string;
-
-    @Prop({ type: String, required: true })
-    organization_id: string;
-
-    @Prop({ type: String, required: true })
-    createdBy: string;
+    @Prop({
+        type: [
+            {
+                document_name: {
+                    type: String,
+                    enum: Object.values(RequiredDocumentTitle),
+                    required: false,
+                },
+                document_link: { type: String, required: false },
+            },
+        ],
+        default: [],
+    })
+    required_documents?: RequiredDocument[]; 
 
     @Prop({ type: Date, default: Date.now })
-    created_at: Date;
+    created_at?: Date;
+
+    @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User', required: false })
+    createdBy?: MongooseSchema.Types.ObjectId;
 }
 
-export const ScholarshipSchema = SchemaFactory.createForClass(Scholarship); 
+export const StudentScholarshipSchema = SchemaFactory.createForClass(StudentScholarship);

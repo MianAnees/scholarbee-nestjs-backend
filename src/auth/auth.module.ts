cb69cc0687eb0851e6c5952841c2157f62670f46
mshahzebraza <m.shahzebraza.97@gmail.com>
2025-04-27 12:52:45 +0500

    chore: reducing the access token expiry time to 1 min

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { AuthV1Guard } from './guards/auth-v1.guard';
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
                 secret: configService.get<string>('JWT_SECRET'),
-                signOptions: { expiresIn: '7d' },
+                signOptions: { expiresIn: '60' },
             }),
         }),
     ],

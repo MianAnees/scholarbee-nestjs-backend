bd21aba38ed856deb961c0ab3a3546eb0cc83618
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-04-29 16:28:16 +0500

    feat: resolve nest js DI issues with analytics and event tracking

--- src/programs/programs.module.ts
+++ src/programs/programs.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProgramsController } from './controllers/programs.controller';
 import { ProgramsService } from './services/programs.service';
 import { Program, ProgramSchema } from './schemas/program.schema';
+import { AnalyticsModule } from 'src/analytics/analytics.module';
 
 @Module({
     imports: [
+        AnalyticsModule,
         MongooseModule.forFeature([
             { name: Program.name, schema: ProgramSchema }
         ])

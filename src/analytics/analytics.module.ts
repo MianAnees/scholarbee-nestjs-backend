daf3f7da30ba4ea849a3f1f5abe7348081ac0d92
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-05-02 14:30:15 +0500

    remove the dependency of analytics module on user-even module

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ElasticsearchModule } from '../elasticsearch/elasticsearch.module';
-import { UserEventLoggerService } from './services/user-event-logger.service';
 import { AnalyticsEsInitService } from './services/analytics-es-init.service';
+import { SearchHistoryAnalyticsService } from './services/search-history-analytics.service';
 
 @Module({
   imports: [ElasticsearchModule],
   providers: [
-    UserEventLoggerService, 
+    SearchHistoryAnalyticsService, 
     AnalyticsEsInitService
   ],
   exports: [
-    UserEventLoggerService
+    SearchHistoryAnalyticsService
   ],
 })
 export class AnalyticsModule {} 

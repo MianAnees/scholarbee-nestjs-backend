69ac745300d3428f1bb2e6b5a67cdfcd1c021f6d
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-05-13 15:09:16 +0500

    feat(auth): create a new login-auth request decorator and type

--- src/auth/decorators/auth-req.decorator.ts
+++ src/auth/decorators/auth-req.decorator.ts
@@ -1,5 +1,23 @@
 import { createParamDecorator, ExecutionContext, BadRequestException } from '@nestjs/common';
-import { AuthenticatedRequest } from 'src/auth/types/auth.interface';
+import {
+  AuthenticatedRequest,
+  LoginRequest,
+} from 'src/auth/types/auth.interface';
+
+export const LoginReq = createParamDecorator<
+  undefined,
+  ExecutionContext,
+  LoginRequest
+>((_data, ctx) => {
+  const request = ctx.switchToHttp().getRequest<LoginRequest>();
+  if (!request.user) {
+    throw new BadRequestException(
+      'User object not found in request. Please ensure that the route is protected by the ResourceProtectionGuard.',
+    );
+  }
+  return request;
+}); 
+
 
 export const AuthReq = createParamDecorator<undefined, ExecutionContext, AuthenticatedRequest>(
     (_data, ctx) => {
--- src/auth/types/auth.interface.ts
+++ src/auth/types/auth.interface.ts
@@ -1,16 +1,22 @@
 import { Request } from 'express';
 import { SanitizedUser } from '../auth.service'; // adjust path as needed
 import { ResourceProtectionStrategy } from 'src/auth/strategies/resource-protection.strategy';
+import { LocalAuthenticationStrategy } from '../strategies/local-authentication.strategy';
 
 export interface AccessTokenPayload extends SanitizedUser {
-    sub: string;
-    userId: string;
+  sub: string;
+  userId: string;
 }
 
 export interface RefreshTokenPayload {
-    userId: string;
-    sub: string;
+  userId: string;
+  sub: string;
 }
+
+export interface LoginRequest extends Request {
+  user: Awaited<ReturnType<LocalAuthenticationStrategy['validate']>>;
+}
+
 
 export interface AuthenticatedRequest extends Request {
     user: Awaited<ReturnType<ResourceProtectionStrategy['validate']>>;

32d8cac61e839c2541746ee60287da0b1a10ec21
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-05-14 12:25:38 +0500

    feat(notification): introduce Audience schema to enhance notification audience management

--- src/notification/schemas/notification.schema.ts
+++ src/notification/schemas/notification.schema.ts
@@ -1,5 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
+import { Campus } from 'src/campuses/schemas/campus.schema';
+import { University } from 'src/universities/schemas/university.schema';
+import { User } from 'src/users/schemas/user.schema';
 
 export type NotificationDocument = Notification & Document;
 
@@ -13,7 +16,7 @@ export type NotificationDocument = Notification & Document;
 // Embedded subdocument for each recipient (no _id field)
 @Schema({ _id: false })
 export class Recipient {
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, refPath: 'audience.audienceType' })
   recipientId: string; // The id of the user, campus, university, etc.
 
   @Prop({ type: Boolean, default: false })
@@ -21,6 +24,25 @@ export class Recipient {
 }
 
 export const RecipientSchema = SchemaFactory.createForClass(Recipient);
+
+@Schema({ _id: false })
+export class Audience {
+  @Prop({
+    type: String,
+    required: true,
+    enum: [User.name, University.name, Campus.name],
+  })
+  audienceType: string;
+
+  // ! For Global notifications, recipients is not present
+  @Prop({ type: Boolean, required: true })
+  isGlobal: boolean;
+
+  @Prop({ type: [RecipientSchema], required: false })
+  recipients?: Recipient[];
+}
+
+export const AudienceSchema = SchemaFactory.createForClass(Audience);
 
 @Schema({ timestamps: { createdAt: 'createdAt', updatedAt: false } })
 export class Notification {
@@ -33,20 +55,9 @@ export class Notification {
   @Prop({ type: String, required: true })
   message: string;
 
-  @Prop({
-    type: {
-      audienceType: { type: String, required: true }, // e.g., 'user', 'university', 'campus'
-      // ! For Global notifications, recipients is not present
-      isGlobal: { type: Boolean, required: true }, // true if for all of this type
-      recipients: { type: [RecipientSchema], required: false }, // Only present if isGlobal is false
-    },
-    required: true,
-  })
-  audience: {
-    audienceType: string;
-    isGlobal: boolean;
-    recipients?: Recipient[];
-  };
+  @Prop({ type: AudienceSchema, required: true })
+  audience: Audience;
 }
 
 export const NotificationSchema = SchemaFactory.createForClass(Notification);
+

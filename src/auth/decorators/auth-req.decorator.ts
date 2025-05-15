d8c843ff8ebe19b10811f7bd404b14c5533c43ea
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-05-15 15:14:28 +0500

    refactor(auth): enhance LoginReq and AuthReq decorators for improved type safety and clarity

--- src/auth/decorators/auth-req.decorator.ts
+++ src/auth/decorators/auth-req.decorator.ts
@@ -4,27 +4,41 @@ import {
   LoginRequest,
 } from 'src/auth/types/auth.interface';
 
+
+/**
+ * This decorator is used to get the user object from the request.
+ * This assumes that the route is protected by the LocalAuthenticationGuard.
+ * Therefore, the user object is always expected to be present in the request. Otherwise, it will throw a BadRequestException. (Which should never happen for routes protected by the LocalAuthenticationGuard)
+ */
 export const LoginReq = createParamDecorator<
   undefined,
   ExecutionContext,
   LoginRequest
 >((_data, ctx) => {
-  const request = ctx.switchToHttp().getRequest<LoginRequest>();
-  if (!request.user) {
+  const loginRequest = ctx.switchToHttp().getRequest<LoginRequest>();
+  if (!loginRequest.user) {
     throw new BadRequestException(
       'User object not found in request. Please ensure that the route is protected by the ResourceProtectionGuard.',
     );
   }
-  return request;
+  return loginRequest;
 }); 
 
 
+/**
+ * This decorator is used to get the user object from the request.
+ * This assumes that the route is protected by the ResourceProtectionGuard.
+ * Therefore, the user object is always expected to be present in the request. Otherwise, it will throw a BadRequestException. (Which should never happen for routes protected by the ResourceProtectionGuard)
+ */
 export const AuthReq = createParamDecorator<undefined, ExecutionContext, AuthenticatedRequest>(
     (_data, ctx) => {
-        const request = ctx.switchToHttp().getRequest<AuthenticatedRequest>();
-        if (!request.user) {
+        const authRequest = ctx.switchToHttp().getRequest<AuthenticatedRequest>();
+        if (!authRequest.user) {
             throw new BadRequestException('User object not found in request. Please ensure that the route is protected by the ResourceProtectionGuard.');
         }
-        return request;
+        return authRequest;
     }
 ); 
+
+
+// TODO: RefreshReq
--- src/auth/types/auth.interface.ts
+++ src/auth/types/auth.interface.ts
@@ -2,6 +2,7 @@ import { Request } from 'express';
 import { SanitizedUser } from '../auth.service'; // adjust path as needed
 import { ResourceProtectionStrategy } from 'src/auth/strategies/resource-protection.strategy';
 import { LocalAuthenticationStrategy } from '../strategies/local-authentication.strategy';
+import { Socket } from 'socket.io';
 
 export interface AccessTokenPayload extends SanitizedUser {
   sub: string;
@@ -17,7 +18,12 @@ export interface LoginRequest extends Request {
   user: Awaited<ReturnType<LocalAuthenticationStrategy['validate']>>;
 }
 
-
 export interface AuthenticatedRequest extends Request {
+  user: Awaited<ReturnType<ResourceProtectionStrategy['validate']>>;
+}
+
+export interface AuthenticatedSocket extends Socket {
+  data: {
     user: Awaited<ReturnType<ResourceProtectionStrategy['validate']>>;
+  };
 }

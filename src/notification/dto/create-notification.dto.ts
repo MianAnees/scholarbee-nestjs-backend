81c744d09aa9da71faacff00efdcee8b1a35051b
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-05-19 17:37:38 +0500

    refactor(notification): simplify create-notification DTO by removing unused audience validation and related classes

--- src/notification/dto/create-notification.dto.ts
+++ src/notification/dto/create-notification.dto.ts
@@ -1,55 +1,13 @@
-import { Type } from 'class-transformer';
 import {
   ArrayNotEmpty,
   IsArray,
-  IsBoolean,
-  IsEnum,
   IsNotEmpty,
-  IsOptional,
   IsString,
   MaxLength,
-  ValidateNested,
 } from 'class-validator';
-import { Types } from 'mongoose';
-import { ToObjectId } from 'src/common/transformers/object-id.transformer';
 import { IsObjectId } from 'src/common/validators/object-id.validator';
-import { AudienceType } from '../schemas/notification.schema';
-import { NotificationAudience } from './notification.validator';
-
-export class RecipientDto {
-  @IsObjectId()
-  @ToObjectId()
-  @IsNotEmpty()
-  id: Types.ObjectId;
-
-  @IsBoolean()
-  @IsOptional()
-  isRead?: boolean = false;
-}
-
-export class AudienceDto implements NotificationAudience {
-  // TODO: Ensure that the audienceType is one of the allowed values i.e. the real collection names in the database e.g. University.name, Campus.name, User.name
-  @IsString()
-  @IsNotEmpty()
-  @IsEnum(AudienceType)
-  audienceType: AudienceType;
-
-  @IsBoolean()
-  isGlobal: boolean;
-
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => RecipientDto)
-  @IsOptional()
-  recipients?: RecipientDto[];
-}
 
 export class CreateNotificationDto {
-  // type is currently disabled in the schema
-  // @IsString()
-  // @IsNotEmpty()
-  // type: string;
-
   @IsString()
   @IsNotEmpty()
   @MaxLength(50)
@@ -58,13 +16,6 @@ export class CreateNotificationDto {
   @IsString()
   @IsNotEmpty()
   message: string;
-
-  // @IsAudienceMutuallyExclusive()
-  // @ValidateNested()
-  // @IsObject()
-  // @IsNotEmpty()
-  // @Type(() => AudienceDto)
-  // audience: AudienceDto;
 }
 
 // DTO for global notification creation

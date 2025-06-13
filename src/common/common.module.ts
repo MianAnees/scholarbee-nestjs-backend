d6af6593038fc1249c979821b016cdc00122f19d
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-06-13 11:46:23 +0500

    feat(common): add TestGateway for WebSocket connections and update CommonModule to include it

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,18 +1,22 @@
 import { Module, Global } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PopulateInterceptor } from './interceptors/populate.interceptor';
-import { University, UniversitySchema } from '../universities/schemas/university.schema';
+import {
+  University,
+  UniversitySchema,
+} from '../universities/schemas/university.schema';
 import { Campus, CampusSchema } from '../campuses/schemas/campus.schema';
+import { TestGateway } from './gateway/test.gateway';
 
 @Global()
 @Module({
-    imports: [
-        MongooseModule.forFeature([
-            { name: University.name, schema: UniversitySchema },
-            { name: Campus.name, schema: CampusSchema },
-        ]),
-    ],
-    providers: [PopulateInterceptor],
-    exports: [PopulateInterceptor],
+  imports: [
+    MongooseModule.forFeature([
+      { name: University.name, schema: UniversitySchema },
+      { name: Campus.name, schema: CampusSchema },
+    ]),
+  ],
+  providers: [PopulateInterceptor, TestGateway],
+  exports: [PopulateInterceptor],
 })
-export class CommonModule { } 
+export class CommonModule {}
---
+++ src/common/gateway/test.gateway.ts
@@ -0,0 +1,21 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ namespace: '/test' })
+export class TestGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('Client connected:', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client.id);
+  }
+}

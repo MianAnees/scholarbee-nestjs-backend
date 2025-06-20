849df6620380bc364af6e893195002a6bf0c478a
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2025-06-20 11:19:34 +0500

    fix(contact): made the `is_scholarship` query-parameter optional
    - Added IsValidBoolean decorator for is_scholarship field to ensure valid boolean values.
    - Reformatted the import statements for better readability.
    - Ensured all fields are properly marked as optional where applicable, improving the flexibility of the DTO.

--- src/contact-us/dto/create-contact.dto.ts
+++ src/contact-us/dto/create-contact.dto.ts
@@ -1,47 +1,57 @@
-import { IsArray, IsBoolean, IsEmail, IsEnum, IsMongoId, IsOptional, IsString } from 'class-validator';
+import {
+  IsArray,
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsMongoId,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { IsValidBoolean } from 'src/auth/decorators/is-valid-boolean.decorator';
 
 export class CreateContactDto {
-    @IsString()
-    name: string;
+  @IsString()
+  name: string;
 
-    @IsEmail()
-    email: string;
+  @IsEmail()
+  email: string;
 
-    @IsString()
-    phone: string;
+  @IsString()
+  phone: string;
 
-    @IsEnum(['registration', 'general'])
-    type: string;
+  @IsEnum(['registration', 'general'])
+  type: string;
 
-    @IsBoolean()
-    is_scholarship: boolean;
+  @IsOptional()
+  @IsValidBoolean()
+  is_scholarship?: boolean;
 
-    @IsOptional()
-    @IsString()
-    message?: string;
+  @IsOptional()
+  @IsString()
+  message?: string;
 
-    @IsOptional()
-    @IsEnum(['Male', 'Female', 'Other'])
-    gender?: string;
+  @IsOptional()
+  @IsEnum(['Male', 'Female', 'Other'])
+  gender?: string;
 
-    @IsOptional()
-    @IsString()
-    study_level?: string;
+  @IsOptional()
+  @IsString()
+  study_level?: string;
 
-    @IsOptional()
-    @IsString()
-    study_country?: string;
+  @IsOptional()
+  @IsString()
+  study_country?: string;
 
-    @IsOptional()
-    @IsString()
-    study_city?: string;
+  @IsOptional()
+  @IsString()
+  study_city?: string;
 
-    @IsOptional()
-    @IsArray()
-    @IsMongoId({ each: true })
-    campusesIds?: string[];
+  @IsOptional()
+  @IsArray()
+  @IsMongoId({ each: true })
+  campusesIds?: string[];
 
-    @IsString()
-    @IsEnum(['Student', 'Admin'])
-    user_type: string;
-} 
+  @IsString()
+  @IsEnum(['Student', 'Admin'])
+  user_type: string;
+}
